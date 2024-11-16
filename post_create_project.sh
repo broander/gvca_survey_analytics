8b2c62cff8aa9090edf6da2571292052b97309b7
Jason Broander <66220841+broander@users.noreply.github.com>
2024-11-16 22:57:23 +0000

    mamba clean added to setup scripts

--- post_create_project.sh
+++ post_create_project.sh
@@ -11,6 +11,7 @@ shopt -s expand_aliases
 # create conda env called "project" and install requirements from environment.yml
 if [ -f './environment.yml' ]; then
     umask 0002 && mamba create --name project --clone base -y && mamba env update -n project -f ./environment.yml
+    mamba clean --all -y
 else
     umask 0002 && mamba create --name project --clone base -y
 fi
