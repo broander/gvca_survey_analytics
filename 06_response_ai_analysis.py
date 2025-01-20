7a5c0c5989f624ab60c705b04c00d093f827ccfb
Jason Broander <66220841+broander@users.noreply.github.com>
2025-01-20 03:14:07 +0000

    subprocess functional

--- 06_response_ai_analysis.py
+++ 06_response_ai_analysis.py
@@ -3,7 +3,9 @@ Access chatgpt from the cli and analyze open response data
 """
 
 import datetime
+import re
 import subprocess
+import sys
 
 from sqlalchemy import create_engine, text
 
@@ -46,6 +48,7 @@ OPENAI_PROMPT_CONTEXT = """
     Communication, Community, Extra-curriculars & Sports, Facilities, and
     Other.
     """
+
 # specify optional prompt history file to load
 HISTORY_FILE_NAME = ""
 # HISTORY_FILE_NAME = "2025-01-13_04-07-37_ai_response_analysis.json"
@@ -56,6 +59,21 @@ OUTPUT_FILES_LOCATION = "./response_analysis_logs/"
 OUTPUT_FILE_NAME = datetime.datetime.now().strftime(
     "%Y-%m-%d_%H-%M-%S" + "_ai_response_analysis"
 )
+
+CHAT_SUBPROCESS_ARGS = [
+    "python",
+    AI_SCRIPT_PATH,
+    "--model",
+    OPENAI_MODEL_NAME,
+    "--context",
+    OPENAI_PROMPT_CONTEXT,
+    "--history",
+    HISTORY_FILE_NAME,
+    "--file",
+    OUTPUT_FILE_NAME,
+    "--location",
+    OUTPUT_FILES_LOCATION,
+]
 
 # load environment variables from .env file
 INPUT_FILEPATH, DATABASE_SCHEMA, DATABASE_CONNECTION_STRING = load_env_vars()
@@ -79,16 +97,81 @@ def query_database(database_connection_string, query, database_schema=None):
         return result.fetchall()
 
 
+def chat_subprocess(args_list):
+    """
+    Call the hello_gpt_assistant.py script as a subprocess
+    Input: args_list - list of arguments to pass to the subprocess
+    Assumed to be the same arguments as the hello_gpt_assistant.py script
+    """
+    # call hello_gpt_assistant as subprocess with the arguments to analyze the
+    # responses.  This allows this script to provide inputs to the user input
+    # prompts and receive the AI responses as output
+
+    # define the pattern to match the subprocess prompt
+    # should match patterh of "1>", "2>", "3>", etc. like in the
+    # hello_gpt_assistant.py
+    subprocess_prompt_pattern = re.compile(r"\d+\> ")
+
+    with subprocess.Popen(
+        args_list,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        bufsize=0,
+        text=True,
+    ) as process:
+
+        buffer = ""
+
+        # Read the output in real-time
+        while True:
+            char = process.stdout.read(1)
+            if not char:
+                # EOF reached
+                break
+
+            # add the character to the buffer
+            buffer += char
+
+            # print the char so we see what's happening
+            sys.stdout.write(char)
+            sys.stdout.flush()
+
+            # check if hte line matches the subprocess prompt pattern
+            match = subprocess_prompt_pattern.search(buffer)
+            if match:
+                # Prompt the user for input back to the subprocess
+                user_input = input("\nInput> ")
+                process.stdin.write(user_input + "\n")
+                process.stdin.flush()
+
+                # Clear the buffer
+                buffer = ""
+
+        # Check for any errors
+        stderr = process.stderr.read()
+        if stderr:
+            print("Errors from subprocess:")
+            print(stderr.decode())
+
+        # Optionally wait for the process to end and get exit code
+        # return_code = process.wait()
+        # print(f"Subprocess finished with return code {return_code}")
+
+
 def analyze_responses(
     database_connection_string=DATABASE_CONNECTION_STRING,
     query=DATABASE_QUERY,
     schema=DATABASE_SCHEMA,
-    script_path=AI_SCRIPT_PATH,
-    history_file=HISTORY_FILE_NAME,
+    subprocess_args=CHAT_SUBPROCESS_ARGS,
 ):
     """
     Analyze the open responses using the OpenAI API
     """
+
+    # prompt engineering pipeline:
+
+    # 1. build prompt to submit the responses data to the AI
 
     # get the open responses from the database
     open_responses = query_database(database_connection_string, query, schema)
@@ -98,58 +181,26 @@ def analyze_responses(
     for response in open_responses:
         prompt += f"- {response[0]}\n"
 
-    # save the prompt to a file
+    # save the prompt to a file so can pass to the subprocess as an argument
+    # to avoid issues with very long prompt data inputs
     # TODO: consider refactor to use a temporary file
     # TODO: refactor file path to align with TODOs on save file functions
     temp_prompt_file_name = "temp_prompt_file"
     temp_prompt_file_path = temp_prompt_file_name
     temp_prompt_file_full_name = temp_prompt_file_path + ".txt"
     save_prompt_file(prompt, temp_prompt_file_path)
-    # breakpoint()
 
-    # uses the prompt context provided as a global variable and
-    # takes the responses as the first user prompt input before continuing
-    # the chat session so you can provie futher direction to the AI
+    # add the prompt file to the subprocess arguments
+    subprocess_args.append("--prompt_file")
+    subprocess_args.append(temp_prompt_file_full_name)
 
-    # call hello_gpt_assistant as subprocess with the arguments to analyze the
-    # responses.  This allows this script to provide inputs to the user input
-    # prompts and receive the AI responses as output
-    process = subprocess.Popen(
-        [
-            "python",
-            script_path,
-            "--model",
-            OPENAI_MODEL_NAME,
-            "--context",
-            OPENAI_PROMPT_CONTEXT,
-            "--history",
-            history_file,
-            "--file",
-            OUTPUT_FILE_NAME,
-            "--location",
-            OUTPUT_FILES_LOCATION,
-            "--prompt_file",
-            temp_prompt_file_full_name,
-        ],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    )
+    # 2. call the chatgpt subprocess to analyze the responses, using the
 
-    # Read the output in real-time
-    while True:
-        output = process.stdout.readline()
-        if output == "" and process.poll() is not None:
-            break
-        if output:
-            print(output.strip())
+    # prompt context provided as a global variable and taking the responses
+    chat_subprocess(subprocess_args)
 
-    # Check for any errors
-    stderr = process.stderr.read()
-    if stderr:
-        print("Errors from subprocess:")
-        print(stderr.decode())
+    # 3. Continue to chat with the AI to refine the analysis and save the
+    # results to a file for later use
 
     # cleanup the temporary prompt file
     subprocess.run(
