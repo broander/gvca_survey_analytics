411aa6f9c370ea3c57f733409a718a915ec048c7
Jason Broander <66220841+broander@users.noreply.github.com>
2025-02-02 04:07:20 +0000

    open response sql

---
+++ export_open_responses_only.sql
@@ -0,0 +1,32 @@
+-- Run this command to output to CSV:
+-- psql -d gvca_survey -U gvcaadmin -f export_open_responses.sql > open_responses.csv
+
+SET search_path TO sac_survey_2025;
+COPY (
+-- response_open
+WITH all_respondent_questions AS
+         (
+             SELECT respondent_id,
+                    question_id,
+                    question_text
+             FROM respondents
+                      CROSS JOIN
+                  questions
+             WHERE question_type = 'open response'
+               AND NOT soft_delete
+         )
+SELECT respondent_id,
+       question_id,
+       question_text,
+       CASE
+           WHEN grammar THEN 'Grammar'
+           WHEN middle THEN 'Middle'
+           WHEN high THEN 'High'
+           WHEN whole_school THEN 'Whole School'
+           END AS grade_level,
+       response
+FROM all_respondent_questions
+         LEFT JOIN
+     question_open_responses USING (respondent_id, question_id)
+ORDER BY respondent_id, question_id, grammar DESC, middle DESC, high DESC, whole_school DESC
+) TO STDOUT WITH CSV HEADER;
